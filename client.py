2ddb2bbcc3550f5841944429c3e57ea470cc8a76
Christian Watson <chris23w@gmail.com>
2022-04-22 21:09:32 -0500

    async calls, tests working

--- client.py
+++ client.py
@@ -1,145 +1,195 @@
 import requests
-from typing import List, Dict, Union
+import aiohttp
+import asyncio
+from typing import List, Dict, Union, Optional
 
 
 class LOTRClient:
 
     BASE_URL = 'https://the-one-api.dev/v2/'
-    FAILURE_MESSAGE = [{
+    FAILURE_MESSAGE = {
         "success": False,
         "message": "Something went wrong.",
-    }]
+    }
 
     def __init__(self, key: str) -> None:
         self._key = key
 
     """Returns a list of the LOTR Books"""
-    def books() -> List[Dict[str, str]]:
-        res = requests.get(LOTRClient.BASE_URL + 'book')
-        return res.json()['docs']
+
+    async def books(self, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, str]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + f'book{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}') as res:
+                result = await res.json()
+                return result['docs']
 
     """Returns a book with the given id"""
-    def book(id: str) -> Dict[str, str]:
-        res = requests.get(LOTRClient.BASE_URL + 'book/' + id)
-        if res.status_code == 200:
-            return res.json()['docs'][0]
-        else:
-            return res.json()
+
+    async def book(self, id: str) -> Dict[str, str]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'book/' + id) as res:
+
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs'][0]
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns all chapters of a book with the given id"""
-    def chapters(id: str) -> List[Dict[str, str]]:
-        res = requests.get(LOTRClient.BASE_URL + 'book/' + id + '/chapter')
-        if res.status_code == 200:
-            return res.json()['docs']
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+
+    async def book_chapters(self, id: str, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, str]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'book/' + id +
+                                   f'/chapter{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}') as res:
+
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs']
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns all movies, including the 'Lord of the Rings' and 'The Hobbit' trilogies"""
 
-    def movies(self) -> List[Dict[str, Union[str, int]]]:
-        res = requests.get(LOTRClient.BASE_URL + 'movie',
-                           headers={'Authorization': f'Bearer {self._key}'})
-        if res.status_code == 200:
-            return res.json()['docs']
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+    async def movies(self, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, Union[str, int]]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + f'movie{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}',
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
+
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs']
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns the details of a movie with the given id"""
 
-    def movie(self, id: str) -> Dict[str, Union[str, int]]:
-        res = requests.get(LOTRClient.BASE_URL + 'movie/' + id,
-                           headers={'Authorization': f'Bearer  {self._key}'})
+    async def movie(self, id: str) -> Dict[str, Union[str, int]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'movie/' + id,
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs'][0]
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs'][0]
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns the quotes from a movie with the given id. 
         Only original LOTR Trilogy is supported"""
 
-    def movie_quotes(self, id: str) -> List[Dict[str, str]]:
-        res = requests.get(LOTRClient.BASE_URL + 'movie/' + id +
-                           '/quote', headers={'Authorization': f'Bearer {self._key}'})
+    async def movie_quotes(self, id: str, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, str]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'movie/' + id +
+                                   f'/quote{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}',
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs']
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs']
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns the list of characters including metadata"""
 
-    def characters(self) -> List[Dict[str, str]]:
-        res = requests.get(LOTRClient.BASE_URL + 'character',
-                           headers={'Authorization': f'Bearer {self._key}'})
+    async def characters(self, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, str]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + f'character{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}',
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs']
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs']
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns the details of a character with the given id"""
 
-    def character(self, id: str) -> Dict[str, str]:
-        res = requests.get(LOTRClient.BASE_URL + 'character/' +
-                           id, headers={'Authorization': f'Bearer {self._key}'})
+    async def character(self, id: str) -> Dict[str, str]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'character/' +
+                                   id, headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs'][0]
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs'][0]
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns the quotes of a character with the given id"""
 
-    def character_quotes(self, id: str) -> List[Dict[str, str]]:
-        res = requests.get(LOTRClient.BASE_URL + 'character/' +
-                           id + '/quote', headers={'Authorization': f'Bearer {self._key}'})
+    async def character_quotes(self, id: str, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, str]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'character/' +
+                                   id +
+                                   f'/quote{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}',
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs']
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs']
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns all movie quotes"""
 
-    def quotes(self) -> List[Dict[str, str]]:
-        res = requests.get(LOTRClient.BASE_URL + 'quote/',
-                           headers={'Authorization': f'Bearer {self._key}'})
+    async def quotes(self, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, str]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + f'quote{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}',
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs']
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs']
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns the details of a quote with the given id"""
 
-    def quote(self, id: str) -> Dict[str, str]:
-        res = requests.get(LOTRClient.BASE_URL + 'quote/' + id,
-                           headers={'Authorization': f'Bearer {self._key}'})
+    async def quote(self, id: str) -> Dict[str, str]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'quote/' + id,
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs'][0]
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs'][0]
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns a list of all book chapters"""
 
-    def chapters(self) -> List[Dict[str, str]]:
-        res = requests.get(LOTRClient.BASE_URL + 'chapter',
-                           headers={'Authorization': f'Bearer {self._key}'})
+    async def chapters(self, options: Optional[Dict[str, Union[str, int]]] = None) -> List[Dict[str, str]]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + f'chapter{"?" + ",".join(f"{key}={value}" for key, value in options) if options else ""}',
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs']
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs']
+                else:
+                    return self.FAILURE_MESSAGE
 
     """Returns the details of a book chapter with the given id"""
 
-    def chapter(self, id: str) -> Dict[str, str]:
-        res = requests.get(LOTRClient.BASE_URL + 'chapter/' + id,
-                           headers={'Authorization': f'Bearer {self._key}'})
+    async def chapter(self, id: str) -> Dict[str, str]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.BASE_URL + 'chapter/' + id,
+                                   headers={'Authorization': f'Bearer {self._key}'}) as res:
 
-        if res.status_code == 200:
-            return res.json()['docs'][0]
-        else:
-            return LOTRClient.FAILURE_MESSAGE
+                result = await res.json()
+
+                if result != self.FAILURE_MESSAGE:
+                    return result['docs'][0]
+                else:
+                    return self.FAILURE_MESSAGE
---
+++ test_lotr_sdk.py
@@ -0,0 +1,178 @@
+from client import LOTRClient
+import requests
+from aiohttp import web
+import pytest
+import aiohttp
+
+
+class MockResponse:
+
+    def __init__(self, res, status_code) -> None:
+        self.res = res
+        self.status_code = status_code
+
+    async def json(self):
+        return self.res
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
+
+    async def __aenter__(self):
+        return self
+
+
+@pytest.fixture
+def mock_response_list(monkeypatch):
+
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    def mock_get(*args, **kwargs):
+        return MockResponse(mock_response, 200)
+
+    monkeypatch.setattr('aiohttp.ClientSession.get', mock_get)
+
+
+@pytest.fixture
+def mock_response_detail(monkeypatch):
+
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing'}
+    ]}
+
+    def mock_get(*args, **kwargs):
+        return MockResponse(mock_response, 200)
+
+    monkeypatch.setattr('aiohttp.ClientSession.get', mock_get)
+
+
+async def test_books(mock_response_list) -> None:
+
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.books() == mock_response['docs']
+
+
+async def test_book_detail(mock_response_detail) -> None:
+
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.book('123') == mock_response['docs'][0]
+
+
+async def test_book_chapters(mock_response_list) -> None:
+
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.book_chapters('123456') == mock_response['docs']
+
+
+async def test_movies(mock_response_list) -> None:
+
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.movies() == mock_response['docs']
+
+
+async def test_movie_detail(mock_response_detail) -> None:
+
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.movie('1234') == mock_response['docs'][0]
+
+
+async def test_movie_quotes(mock_response_list) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.movie_quotes('1234') == mock_response['docs']
+
+
+async def test_characters(mock_response_list) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.characters() == mock_response['docs']
+
+
+async def test_character_detail(mock_response_detail) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.character('1234') == mock_response['docs'][0]
+
+
+async def test_character_quotes(mock_response_list) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.character_quotes('1234') == mock_response['docs']
+
+
+async def test_quotes(mock_response_list) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.quotes() == mock_response['docs']
+
+
+async def test_quote_detail(mock_response_detail) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.quote('12345675') == mock_response['docs'][0]
+
+
+async def test_chapters(mock_response_list) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing1'},
+        {'id': '124', 'name': 'thing2'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.chapters() == mock_response['docs']
+
+
+async def test_chapter_detail(mock_response_detail) -> None:
+    mock_response = {'docs': [
+        {'id': '123', 'name': 'thing'}
+    ]}
+
+    client = LOTRClient('fakekey')
+    assert await client.chapter('1234') == mock_response['docs'][0]
